6730272665351f0a6d8907edd515263188df0166
bbakercello <bbakercello@gmail.com>
2022-10-24 14:35:10 -0400

    mock data created

--- main_app/views.py
+++ main_app/views.py
@@ -11,3 +11,20 @@ class Home(TemplateView):
 
 class List(TemplateView):
         template_name='list.html'
+
+        def get_context_data(self, **kwargs):
+            context = super().get_context_data(**kwargs)
+            context["lists"] = lists
+            return context
+
+class List:
+    def __init__(self,owner,items):
+        self.owner = owner
+        self.items = items
+
+lists = [
+    List("Ben Baker",['apple','orange','chicken thighs','yogurt']),
+    List("Brenna Langille", ['pickles','green cola','sausages','peppers'],
+    List("Devin Larsen",['tempeh','grains','ointment','toothpaste']))
+]
+
